6e17db43bb048ea8f570fba80010ca5db3aedf4b
Ricardo Fuzeto <ricardo.fuzeto@monitoratec.com.br>
2019-12-02 16:26:49 -0300

    Initial page commit

--- src/colorMath.js
+++
@@ -1,8 +0,0 @@
-const calculateShadedColor = (factor, color) => {
-  const [r, g, b] = color.match(/(\d+)/g);
-  return `rgb(${Math.round(r * (1 + factor))},${Math.round(g * (1 + factor))},${Math.round(b * (1 + factor))})`;
-};
-
-export const darken = (color, percentage) => calculateShadedColor(-1 * percentage, color);
-
-export const lighten = (color, percentage) => calculateShadedColor(percentage, color);
